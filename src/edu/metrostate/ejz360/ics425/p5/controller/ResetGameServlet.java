0b0dc999a02477d86c668183dacf0e3687f2e55a
edzempel <edward.zempel@gmail.com>
2019-11-26 00:20:05 -0600

    add ResetGameServlet and Game.java

--- src/edu/metrostate/ejz360/ics425/p5/controller/ResetGameServlet.java
+++ src/edu/metrostate/ejz360/ics425/p5/controller/ResetGameServlet.java
@@ -7,35 +7,50 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import edu.metrostate.ejz360.ics425.p5.model.Game;
+
 /**
  * Servlet implementation class ResetGameServlet
  */
 @WebServlet("/resetGame")
 public class ResetGameServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ResetGameServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	public ResetGameServlet() {
+		super();
+
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		processRequest(request, response);
+	}
+
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		((Game) request.getSession().getAttribute("game")).reset();
+
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+
 	}
 
 }
---
+++ test/edu/metrostate/ejz360/ics425/p5/model/Game.java
@@ -0,0 +1,19 @@
+package edu.metrostate.ejz360.ics425.p5.model;
+
+import java.io.Serializable;
+
+public class Game implements Serializable {
+
+	/**
+	 * Version number for Game bean
+	 */
+	private static final long serialVersionUID = 20191125L;
+
+	/**
+	 * Initializes new game
+	 */
+	public void reset() {
+		// TODO: create game initializer
+	}
+
+}
