9dd863a1927db32988628c1f5efd879e1334b258
edzempel <edward.zempel@gmail.com>
2019-12-07 15:49:44 -0600

    extract Guess

---
+++ src/edu/metrostate/ejz360/ics425/p5/model/Guess.java
@@ -0,0 +1,56 @@
+package edu.metrostate.ejz360.ics425.p5.model;
+
+import java.io.Serializable;
+
+public class Guess implements Serializable {
+
+	private static final long serialVersionUID = 20191207L;
+	private static final int GUESS_SIZE = 4;
+	private CodePeg[] guessPegs = new CodePeg[GUESS_SIZE];
+
+	public Guess() {
+		super();
+	}
+
+	public CodePeg[] getGuessPegs() {
+		return guessPegs;
+	}
+
+	public void setGuessPegs(CodePeg one, CodePeg two, CodePeg three, CodePeg four) {
+		guessPegs[0] = one;
+		guessPegs[1] = two;
+		guessPegs[2] = three;
+		guessPegs[3] = four;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		boolean equal = false;
+
+		if (obj instanceof Guess) {
+			CodePeg[] objGuessPegs = ((Guess) obj).getGuessPegs().clone();
+			for (int i = 0; i < this.guessPegs.length; i++) {
+				equal = this.guessPegs[i].equals(objGuessPegs[i]);
+				if (!equal) {
+					break;
+				}
+			}
+		}
+		return equal;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s,%s,%s,%s", guessPegs[0], guessPegs[1], guessPegs[2], guessPegs[3]);
+	}
+
+	public enum CodePeg {
+		RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE
+
+	}
+
+	public enum KeyPeg {
+		WHITE, BLACK
+	}
+
+}
