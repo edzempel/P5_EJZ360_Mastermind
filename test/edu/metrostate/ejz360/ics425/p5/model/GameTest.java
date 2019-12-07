88c8b2feeda343399699c5a53e76c067754fc049
edzempel <edward.zempel@gmail.com>
2019-12-07 15:49:52 -0600

    Game and Guess Tests

---
+++ test/edu/metrostate/ejz360/ics425/p5/model/GameTest.java
@@ -0,0 +1,60 @@
+package edu.metrostate.ejz360.ics425.p5.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import edu.metrostate.ejz360.ics425.p5.model.Guess.CodePeg;
+import edu.metrostate.ejz360.ics425.p5.model.Guess;
+
+class GameTest {
+	Game game;
+	Guess guess1;
+	Guess guess2;
+	Guess guess3;
+	Guess guess4;
+	Guess makerCode;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		game = new Game();
+		guess1 = new Guess();
+		guess1.setGuessPegs(CodePeg.BLUE, CodePeg.GREEN, CodePeg.ORANGE, CodePeg.PURPLE);
+		guess2 = new Guess();
+		guess2.setGuessPegs(CodePeg.RED, CodePeg.YELLOW, CodePeg.BLUE, CodePeg.BLUE);
+		guess3 = new Guess();
+		guess3.setGuessPegs(CodePeg.GREEN, CodePeg.RED, CodePeg.YELLOW, CodePeg.ORANGE);
+		guess4 = new Guess();
+		guess4.setGuessPegs(CodePeg.BLUE, CodePeg.GREEN, CodePeg.PURPLE, CodePeg.RED);
+		makerCode = new Guess();
+		makerCode.setGuessPegs(CodePeg.BLUE, CodePeg.GREEN, CodePeg.PURPLE, CodePeg.RED);
+	}
+
+	@Test
+	void testDefaults() {
+		assertTrue(game.getGuesses().isEmpty());
+		assertEquals(12, Game.NUM_GUESSES);
+		assertTrue(game instanceof Game);
+		assertFalse(game.isOver());
+	}
+
+	@Test
+	void testAddGuess() {
+		game.addGuess(guess1);
+		game.addGuess(guess2);
+		game.addGuess(guess3);
+
+		assertEquals(guess1, game.getGuesses().get(0));
+		assertEquals(guess2, game.getGuesses().get(1));
+		assertEquals(guess3, game.getGuesses().get(2));
+		assertNotEquals(guess4, game.getGuesses().get(2));
+	}
+
+	@Test
+	void testIsWinner() {
+		assertNotEquals(makerCode, guess3);
+		assertEquals(makerCode, guess4);
+	}
+
+}
---
+++ test/edu/metrostate/ejz360/ics425/p5/model/GuessTest.java
@@ -0,0 +1,53 @@
+package edu.metrostate.ejz360.ics425.p5.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import edu.metrostate.ejz360.ics425.p5.model.Guess.CodePeg;
+
+class GuessTest {
+	Guess guess1;
+	Guess guess2;
+	Guess guess3;
+	Guess guess4;
+	Guess guessNull;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		guess1 = new Guess();
+		guess1.setGuessPegs(CodePeg.BLUE, CodePeg.GREEN, CodePeg.ORANGE, CodePeg.PURPLE);
+		guess2 = new Guess();
+		guess2.setGuessPegs(CodePeg.RED, CodePeg.YELLOW, CodePeg.BLUE, CodePeg.BLUE);
+		guess3 = new Guess();
+		guess3.setGuessPegs(CodePeg.GREEN, CodePeg.RED, CodePeg.YELLOW, CodePeg.ORANGE);
+		guess4 = new Guess();
+		guess4.setGuessPegs(CodePeg.GREEN, CodePeg.RED, CodePeg.YELLOW, CodePeg.ORANGE);
+	}
+
+	@Test
+	void testGuess() {
+		assertNull(guessNull);
+	}
+
+	@Test
+	void testGetGuessPegs() {
+		assertEquals(CodePeg.BLUE, guess1.getGuessPegs()[0]);
+		assertEquals(CodePeg.GREEN, guess1.getGuessPegs()[1]);
+		assertEquals(CodePeg.ORANGE, guess1.getGuessPegs()[2]);
+		assertEquals(CodePeg.PURPLE, guess1.getGuessPegs()[3]);
+	}
+
+	@Test
+	void testEqualsObject() {
+		assertNotEquals(guess2, guess3);
+		assertEquals(guess3, guess4);
+	}
+	
+	@Test
+	void testToString() {
+		assertEquals("BLUE,GREEN,ORANGE,PURPLE", guess1.toString());
+	}
+
+}
